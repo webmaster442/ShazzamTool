048773248d93d8941e32e6ecc8ad82b25f082b5f
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-02-07 17:56:49 +0100

    Changes to be able to release

--- Shazzam/Shazzam/Helpers/VersionHelper.cs
+++ Shazzam/Shazzam/Helpers/VersionHelper.cs
@@ -12,20 +12,9 @@ namespace Shazzam.Helpers
 
             string ourVersion = string.Empty;
 
-            //if running the deployed application, you can get the version
-            //  from the ApplicationDeployment information. If you try
-            //  to access this when you are running in Visual Studio, it will not work.
-
-            if (System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed)
+            if (_assemblyInfo != null)
             {
-                ourVersion = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
-            }
-            else
-            {
-                if (_assemblyInfo != null)
-                {
-                    ourVersion = _assemblyInfo.GetName().Version.ToString();
-                }
+                ourVersion = _assemblyInfo.GetName().Version.ToString();
             }
             return ourVersion;
 
@@ -36,24 +25,11 @@ namespace Shazzam.Helpers
 
             string ourVersion = string.Empty;
 
-            // if running the deployed application, you can get the version
-            // from the ApplicationDeployment information. If you try
-            // to access this when you are running in Visual Studio, it will not work.
-            if (System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed)
+            if (_assemblyInfo != null)
             {
-                // ourVersion = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
                 ourVersion = String.Format("{0}.{1}",
-                                     ApplicationDeployment.CurrentDeployment.CurrentVersion.Major.ToString(),
-                                     ApplicationDeployment.CurrentDeployment.CurrentVersion.Minor.ToString());
-            }
-            else
-            {
-                if (_assemblyInfo != null)
-                {
-                    ourVersion = String.Format("{0}.{1}",
-                                                _assemblyInfo.GetName().Version.Major.ToString(),
-                                                _assemblyInfo.GetName().Version.Minor.ToString());
-                }
+                                            _assemblyInfo.GetName().Version.Major.ToString(),
+                                            _assemblyInfo.GetName().Version.Minor.ToString());
             }
             return ourVersion;
 
---
+++ changes.md
@@ -0,0 +1,6 @@
+# Version 1.5
+ * Forked code from the CodePlex archive https://archive.codeplex.com/?p=shazzam
+ * Updated target framework to .NET 4.7
+ * Minor source changes to be able to compile with VS 2017 Community
+ * Unit test project not adapted
+ * Help menu links changed to point to new site
