b136bb37c7956dc2a74616255e403f82ed5a564a
webmaster442 <ruzsinszki.gabor@gmail.com>
2019-02-07 13:01:37 +0100

    Original sources

---
+++ Shazzam/Shazzam/ViewModels/CodeTabViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shazzam.ViewModels
+{
+  class CodeTabViewModel : ViewModelBase
+  {
+
+    public bool AreGeneratedCodeTabsEnabled
+    {
+      get
+      {
+        return Shazzam.Properties.Settings.Default.AreGeneratedCodeTabsEnabled;
+      }
+    }
+
+  }
+}
